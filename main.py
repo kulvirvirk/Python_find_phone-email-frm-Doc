fe41d84a7983669775bb611ada4079da8d4359c3
kulvirvirk <kvirkdev@gmail.com>
2020-05-10 04:26:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# If you have a document from which you have to find the all the emails and phone numbers. 
+
+# see the example attached with this code and find all the emails and phone numbers from that doc.
