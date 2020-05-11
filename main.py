09caee79ec00fdbf005aad4571873e7de0885877
kulvirvirk <kvirkdev@gmail.com>
2020-05-11 04:52:06 +0000

    link

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # If you have a document from which you have to find the all the emails and phone numbers. 
 
 # see the example attached with this code and find all the emails and phone numbers from that doc.
+
+# https://www.udemy.com/course/automate/learn/lecture/3470534#overview
